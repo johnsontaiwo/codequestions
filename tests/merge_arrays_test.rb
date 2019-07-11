f787b989dc742ca84664019f9b8c54f5bb130231
johnsontaiwo <josseytee@gmail.com>
2019-07-11 12:42:30 -0400

    solution

---
+++ tests/merge_arrays_test.rb
@@ -0,0 +1,35 @@
+require 'test/unit'
+require_relative '../merge_arrays'
+
+def run_tests
+  desc = 'both arrays are empty'
+  actual = merge_arrays([], [])
+  expected = []
+  assert_equal(actual, expected, desc)
+
+  desc = 'first array is empty'
+  actual = merge_arrays([], [1, 2, 3])
+  expected = [1, 2, 3]
+  assert_equal(actual, expected, desc)
+
+  desc = 'second array is empty'
+  actual = merge_arrays([5, 6, 7], [])
+  expected = [5, 6, 7]
+  assert_equal(actual, expected, desc)
+
+  desc = 'both arrays have some numbers'
+  actual = merge_arrays([2, 4, 6], [1, 3, 7])
+  expected = [1, 2, 3, 4, 6, 7]
+  assert_equal(actual, expected, desc)
+
+  desc = 'arrays are different lengths'
+  actual = merge_arrays([2, 4, 6, 8], [1, 7])
+  expected = [1, 2, 4, 6, 7, 8]
+  assert_equal(actual, expected, desc)
+end
+
+def assert_equal(a, b, desc)
+  puts "#{desc} ... #{a == b ? 'PASS' : "FAIL: #{a.inspect} != #{b.inspect}"}"
+end
+
+run_tests
