3acace723450e3b3eb6ca4d887c3e5b10b52db08
johnsontaiwo <josseytee@gmail.com>
2019-07-17 12:41:28 -0400

    solution

--- first_come_first_served.rb
+++ first_come_first_served.rb
@@ -1,9 +1,20 @@
 def first_come_first_served?(take_out, dine_in, served_orders)
 
-  # Check if we're serving orders first-come, first-served
-   if served_orders.length == take_out.length + dine_in.length
-      return true
+    dine_in_start_index  = 0
+    take_out_start_index = 0
+    
+    served_orders.each do |order|
+      if dine_in.length - 1 >= dine_in_start_index && order == dine_in[dine_in_start_index]
+       dine_in_start_index += 1
+      elsif take_out.length - 1 >= take_out_start_index && order == take_out[take_out_start_index]
+       take_out_start_index += 1
+      else
+      return false
+      end
     end
-
-  false
+  return true
 end
+  
+      
+        
+ 
--- tests/first_come_first_served_test.rb
+++ tests/first_come_first_served_test.rb
@@ -1,1 +1,34 @@
-first_come_first_served_test.rb
+require 'test/unit'
+require_relative '../first_come_first_served'
+
+def run_tests
+  desc = 'both registers have same number of orders'
+  result = first_come_first_served?([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+  assert_true(result, desc)
+
+  desc = 'registers have different lengths'
+  result = first_come_first_served?([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])
+  assert_false(result, desc)
+
+  desc = 'one register is empty'
+  result = first_come_first_served?([], [2, 3, 6], [2, 3, 6])
+  assert_true(result, desc)
+
+  desc = 'served orders is missing orders'
+  result = first_come_first_served?([1, 5], [2, 3, 6], [1, 6, 3, 5])
+  assert_false(result, desc)
+
+  desc = 'served orders has extra orders'
+  result = first_come_first_served?([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8])
+  assert_false(result, desc)
+end
+
+def assert_true(value, desc)
+  puts "#{desc} ... #{value ? 'PASS' : "FAIL: #{value} is not true"}"
+end
+
+def assert_false(value, desc)
+  puts "#{desc} ... #{value ? "FAIL: #{value} is not false" : 'PASS'}"
+end
+
+run_tests
