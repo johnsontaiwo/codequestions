a45ef114a7c75017960a675baa1e723760dcfd28
johnsontaiwo <josseytee@gmail.com>
2019-07-20 09:08:05 -0400

    solved

--- inflight_movies.rb
+++ inflight_movies.rb
@@ -1,7 +1,18 @@
-def can_two_movies_fill_flight?(flight_time, movies_times)
+require 'set'
 
-false
+def can_two_movies_fill_flight?(movies_times, flight_time)
 
+movies_seen = Set.new
+
+movies_times.any? do |first_movie|
+  second_movie = flight_time - first_movie
+  if movies_seen.include?(second_movie)
+      true
+    else
+      movies_seen << first_movie
+      false
+  end
+end
 end
 
 
